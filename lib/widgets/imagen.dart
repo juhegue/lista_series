9cf14ece9a88414f4624bd27d0a4c8b9731fac21
Juan Hevilla <j.hevilla@diagram.es>
2022-12-03 00:33:01 +0100

    Maximizar imagen

--- lib/widgets/imagen.dart
+++ lib/widgets/imagen.dart
@@ -10,41 +10,15 @@ class Imagen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(titulo),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          FittedBox(
-            fit: BoxFit.fill,
-            child: Image.memory(imagen),
-          ),
-          const SizedBox(height: 20.0),
-          Align(
-              alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                height: 45.0,
-                width: 150.0,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.green),
-                  ),
-                  child: const Text(
-                    'Volver',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                    ),
-                  ),
-                ),
-              )),
-          const SizedBox(height: 20.0),
-        ]),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(titulo),
+          centerTitle: true,
+        ),
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+          image: Image.memory(imagen).image,
+          fit: BoxFit.cover,
+        ))));
   }
 }
